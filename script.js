5a26ede54c3c6683d904b7f4911181cf79ae11bc
elverdiaz1016 <diazelver1016@gmail.com>
2024-06-13 11:34:41 -0500

    fix: :bug: clone correction in javascript

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const listCard = [
     {
         id: 1,
-        imgHtml: "/images/html.svg",
+        imgHtml: "images/html.svg",
         imgAlt: "Icon Html",
-        tittleCourse: "hmtl",
+        tittleCourse: "Hmtl",
         nameAutor: "Elver Riaño Diaz",
         analogTime: "6h 30min",
         qualification: "4,9"
     },
     {
         id: 2,
-        imgHtml: "/images/css3.svg",
+        imgHtml: "images/css3.svg",
         imgAlt: "Icon Css",
         tittleCourse: "Css",
         nameAutor: "Elver Riaño Diaz",
@@ -19,7 +19,7 @@ const listCard = [
     },
     {
         id: 3,
-        imgHtml: "/images/javascript.svg",
+        imgHtml: "images/javascript.svg",
         imgAlt: "Icon Js",
         tittleCourse: "JS",
         nameAutor: "Elver Riaño Diaz",
@@ -28,7 +28,7 @@ const listCard = [
     },
     {
         id: 4,
-        imgHtml: "/images/csharp.svg",
+        imgHtml: "images/csharp.svg",
         imgAlt: "Icon c#",
         tittleCourse: "C#",
         nameAutor: "Elver Riaño Diaz",
@@ -37,7 +37,7 @@ const listCard = [
     },
     {
         id: 5,
-        imgHtml: "/images/flutter.svg",
+        imgHtml: "images/flutter.svg",
         imgAlt: "Icon Flutter",
         tittleCourse: "Flutter",
         nameAutor: "Elver Riaño Diaz",
